77a33d9770a6cc9b9934d989a359c330c3852609
Carlos Sosa <cesosag@gmail.com>
2014-10-27 14:44:19 -0500

    Fonts updated, opacity

---
+++ wp-content/themes/mc2015/page-artists.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<?php get_template_part('part', 'header'); ?>
+<div class="wrap">
+	<main role="main">
+	</main>
+</div>
+<?php get_template_part('part', 'footer'); ?>
+<?php get_footer(); ?>
