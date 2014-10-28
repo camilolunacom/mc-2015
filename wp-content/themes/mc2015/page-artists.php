0b5231f7e240fac237a0a3850a67e00717330d1b
Carlos Sosa <cesosag@gmail.com>
2014-10-28 16:02:59 -0500

    Menu for mobiles, markup for artists page

--- wp-content/themes/mc2015/page-artists.php
+++ wp-content/themes/mc2015/page-artists.php
@@ -1,8 +1,24 @@
+<?php
+	$args = array( 'post_type' => 'artist', 'posts_per_page' => 10 );
+	$loop = new WP_Query( $args );
+?>
 <?php get_header(); ?>
 <?php get_template_part('part', 'header'); ?>
 <div class="wrap">
 	<main role="main">
+		<ul class="artists">
+			<?php while ( $loop->have_posts() ) : $loop->the_post();
+				$attachment_id = get_post_thumbnail_id($post_id);
+				$image_attributes = wp_get_attachment_image_src($attachment_id);
+			?>
+				<li class="artist" style="background: url(<?php echo $image_attributes[0]; ?>)">
+					<div class="artist-name">
+						<span><?php echo the_title(); ?></span>
+						<a href="<?php echo the_permalink(); ?>" class="view-artist"><?php _e( 'View artist', 'mor'); ?></a>
+					</div>
+				</li>
+			<?php endwhile; ?>
+		</ul>
 	</main>
 </div>
-<?php get_template_part('part', 'footer'); ?>
 <?php get_footer(); ?>
--- wp-content/themes/mc2015/part-header.php
+++ wp-content/themes/mc2015/part-header.php
@@ -2,11 +2,16 @@
 	<div class="logo">
 		<a class="icon-logo" href="<?php echo home_url(); ?>/" rel="home"></a>
 	</div>
-	<div id="hamburguer" class="icon-menu">-</div>
+	<div id="hamburguer" class="icon-menu"></div>
 	<nav role="navigation">
 		<?php wp_nav_menu(array('theme_location' => 'primary')); ?>
 	</nav>
 	<div class="nav-footer">
+		<ul class="social-networks">
+			<li class="social-network"><a href="" class="icon-facebook"></a></li>
+			<li class="social-network"><a href="" class="icon-twitter"></a></li>
+			<li class="social-network"><a href="" class="icon-instagram"></a></li>
+		</ul>
 		<?php kc_ml_list_languages(false) ?>
 		<?php get_template_part('prod', 'team'); ?>
 	</div>
