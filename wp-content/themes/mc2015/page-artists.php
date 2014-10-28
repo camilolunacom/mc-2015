c65de3294d0c56f7ffccdea345d8cf5b9f8a5538
Carlos Sosa <cesosag@gmail.com>
2014-10-28 17:10:11 -0500

    Artists page styles for mobile

--- wp-content/themes/mc2015/page-artists.php
+++ wp-content/themes/mc2015/page-artists.php
@@ -9,14 +9,14 @@
 		<ul class="artists">
 			<?php while ( $loop->have_posts() ) : $loop->the_post();
 				$attachment_id = get_post_thumbnail_id($post_id);
-				$image_attributes = wp_get_attachment_image_src($attachment_id);
+				$image_attributes = wp_get_attachment_image_src($attachment_id, 'large');
 			?>
-				<li class="artist" style="background: url(<?php echo $image_attributes[0]; ?>)">
-					<div class="artist-name">
-						<span><?php echo the_title(); ?></span>
-						<a href="<?php echo the_permalink(); ?>" class="view-artist"><?php _e( 'View artist', 'mor'); ?></a>
-					</div>
-				</li>
+			<li class="artist" style="background-image: url(<?php echo $image_attributes[0]; ?>)">
+				<div class="artist-name">
+					<span><?php echo the_title(); ?></span>
+					<a href="<?php echo the_permalink(); ?>" class="view-artist"><?php _e( 'View artist', 'mor'); ?></a>
+				</div>
+			</li>
 			<?php endwhile; ?>
 		</ul>
 	</main>
