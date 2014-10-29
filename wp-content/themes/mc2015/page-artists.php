0383c49ed53cbc8fc2380074735ae0bc31d91ca6
Carlos Sosa <cesosag@gmail.com>
2014-10-28 22:24:52 -0500

    Artists page for desktop

--- wp-content/themes/mc2015/page-artists.php
+++ wp-content/themes/mc2015/page-artists.php
@@ -1,20 +1,21 @@
 <?php
-	$args = array( 'post_type' => 'artist', 'posts_per_page' => 10 );
+	$args = array( 'post_type' => 'artist', 'posts_per_page' => 50 );
 	$loop = new WP_Query( $args );
 ?>
 <?php get_header(); ?>
 <?php get_template_part('part', 'header'); ?>
 <div class="wrap">
 	<main role="main">
-		<ul class="artists">
+		<ul class="artists thumb">
 			<?php while ( $loop->have_posts() ) : $loop->the_post();
 				$attachment_id = get_post_thumbnail_id($post_id);
 				$image_attributes = wp_get_attachment_image_src($attachment_id, 'large');
 			?>
-			<li class="artist" style="background-image: url(<?php echo $image_attributes[0]; ?>)">
+			<li class="artist thumb" style="background-image: url(<?php echo $image_attributes[0]; ?>)">
 				<div class="artist-name">
 					<span><?php echo the_title(); ?></span>
 					<a href="<?php echo the_permalink(); ?>" class="view-artist"><?php _e( 'View artist', 'mor'); ?></a>
+					<a href="<?php echo the_permalink(); ?>" class="artist-link"><?php echo the_title(); ?></a>
 				</div>
 			</li>
 			<?php endwhile; ?>
