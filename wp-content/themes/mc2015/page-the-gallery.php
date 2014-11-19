e9776585b66ccdf63fe1951fd7a56f2dfd722054
Carlos Sosa <cesosag@gmail.com>
2014-11-19 14:55:03 -0500

    The gallery first part styling

--- wp-content/themes/mc2015/page-the-gallery.php
+++ wp-content/themes/mc2015/page-the-gallery.php
@@ -3,10 +3,28 @@
 <div class="wrap">
 	<main role="main">
 		<?php while ( have_posts() ) : the_post(); ?>
-			<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-				<h2><?php the_title(); ?></h2>
+			<div <?php post_class(); ?> id="the-gallery">
 				<?php the_content(); ?>
-			</article>
+				<div class="contact-info">
+					<div class="contact-info-city">
+						<h2 class="contact-city-name">Paris</h2>
+						<p class="contact-city-details"><?php _e( 'Tuesday-Saturday / 11am-7pm', 'mor'); ?></p>
+						<p class="contact-city-details">+33 (0) 1 44 54 01 58</p>
+						<a href="mailto:contact@mor-charpentier.com" class="contact-city-mail">contact@mor-charpentier.com</a>
+					</div>
+					<div class="contact-info-city">
+						<h2 class="contact-city-name">Bogotá</h2>
+						<p class="contact-city-details">+57 (1) 618 2746</p>
+						<br />
+						<a href="mailto:bogota@mor-charpentier.com" class="contact-city-mail">bogota@mor-charpentier.com</a>
+					</div>
+				</div>
+				<ul class="social-networks">
+					<li class="social-network"><a href="" class="icon-facebook"></a></li>
+					<li class="social-network"><a href="" class="icon-twitter"></a></li>
+					<li class="social-network"><a href="" class="icon-instagram"></a></li>
+				</ul>
+			</div>
 		<?php endwhile; ?>
 	</main>
 </div>
