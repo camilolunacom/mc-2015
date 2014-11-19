70e945a1bfe0c38dee3c174eaba86b611cd0b644
Anabelle Handdoek <github@huellaspyp.com>
2014-11-19 17:21:45 -0500

    random home image with credits

--- wp-content/themes/mc2015/front-page.php
+++ wp-content/themes/mc2015/front-page.php
@@ -1,12 +1,33 @@
-<?php get_header(); ?>
+<?php 
+	get_header();
+	$_images = kc_get_option( 'mcg', 'gallery', 'images' );
+	if ( isset($_images['selected']) ) { 
+		$img_count = count( $_images['selected'] );
+		$rand_img = rand( 0, $img_count );
+		$img = $_images['selected'][$rand_img];
+		$full = wp_get_attachment_image_src( $img, 'full' );
+	}
+?>
 	<div class="wrap index">
-		<main class="index vertical-align" role="main"   style="background:url(http://dev.mor-charpentier.com/wp-content/uploads/2013/01/Julieta-Aranda-For-I-am-not-sleepy-yet-and-I-dont-want-to-rest.jpg); ?>)">
+		<main class="index vertical-align" role="main"   style="background:url(<?php echo $full[0] ?>); ?>)">
+		
 			<div class="logo">
 				<a class="icon-logo" href="<?php echo home_url(); ?>/" rel="home"></a>
 			</div>
-			<?php kc_ml_list_languages(false) ?>
-			<?php get_template_part('prod', 'team'); ?>
-			<?php get_template_part('picture', 'credits'); ?>
+
+			<?php 
+				kc_ml_list_languages(false); 
+				get_template_part('prod', 'team');
+				$text = get_the_title( $img );
+				preg_match( '#\((.*?)\)#' , $text , $match );
+				$line1 = preg_replace( "/\([^)]+\)/" , "" , $text );
+			?>
+
+			<div class="picture-credits">
+				<span class="picture-artist"><?php echo $line1; ?></span>
+				<span class="picture-title"><?php echo $match[0]; ?></span>
+			</div>
+
 			<div class="index-menu">
 				<nav role="navigation">
 					<?php wp_nav_menu(array('theme_location' => 'primary')); ?>
@@ -18,4 +39,4 @@
 			<?php get_template_part('footer-content'); ?>
 		</main>
 	</div>
-	<?php get_footer(); ?>
+<?php get_footer(); ?>
--- wp-content/themes/mc2015/picture-credits.php
+++
@@ -1,4 +0,0 @@
-<div class="picture-credits">
-	<span class="picture-artist">Óscar Muñoz</span>
-	<span class="picture-title">Cortinas de baño</span>
-</div>
