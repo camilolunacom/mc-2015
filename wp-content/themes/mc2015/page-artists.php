ef2a5536ca737b376d4a7737c90afd93705f68d1
Carlos Sosa <cesosag@gmail.com>
2014-11-25 13:23:48 -0500

    Artist whole clickable

--- wp-content/themes/mc2015/page-artists.php
+++ wp-content/themes/mc2015/page-artists.php
@@ -16,9 +16,10 @@
 				$image_attributes = wp_get_attachment_image_src($attachment_id, 'large');
 			?>
 			<li class="artist thumb" style="background-image: url(<?php echo $image_attributes[0]; ?>)">
+				<a href="<?php echo the_permalink(); ?>"></a>
 				<div class="artist-name">
 					<span><?php echo the_title(); ?></span>
-					<a href="<?php echo the_permalink(); ?>" class="view-artist"><?php _e( 'View artist', 'mor'); ?></a>
+					<div class="view-artist"><?php _e( 'View artist', 'mor'); ?></div>
 					<a href="<?php echo the_permalink(); ?>" class="artist-link"><?php echo the_title(); ?></a>
 				</div>
 			</li>
