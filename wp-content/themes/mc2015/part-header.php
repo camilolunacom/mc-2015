564e0259faccc55dcdd9189797bb86c807341a5d
Carlos Sosa <cesosag@gmail.com>
2014-12-01 16:58:55 -0500

    Social networks

--- wp-content/themes/mc2015/part-header.php
+++ wp-content/themes/mc2015/part-header.php
@@ -7,11 +7,7 @@
 		<?php wp_nav_menu(array('theme_location' => 'primary')); ?>
 	</nav>
 	<div class="nav-footer">
-		<ul class="social-networks">
-			<li class="social-network"><a href="" class="icon-facebook"></a></li>
-			<li class="social-network"><a href="" class="icon-twitter"></a></li>
-			<li class="social-network"><a href="" class="icon-instagram"></a></li>
-		</ul>
+		<?php get_template_part('part', 'social'); ?>
 		<?php kc_ml_list_languages( false, 'language_code' ); ?>
 		<?php get_template_part('prod', 'team'); ?>
 	</div>
