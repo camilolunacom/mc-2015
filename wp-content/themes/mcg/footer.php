0a5bcd2ebe7da1e313f3e207165e04fbf3f195bf
Carlos Sosa <cesosag@gmail.com>
2015-02-04 10:45:08 -0500

    Removed old theme, added favicons

--- wp-content/themes/mcg/footer.php
+++
@@ -1,7 +0,0 @@
-		<footer id="colophon">
-      <p id=credit><span>Designed by:</span> <a href="http://8manos.com" class="im"><span>8manos</span></a> <span>+</span> <a href="http://elmonocromo.com/" class="mc"><span>monocromo</span></a></p>
-		</footer>
-	</div>
-	<?php wp_footer() ?>
-</body>
-</html>
